1df7129d041f00b1ace453584a53f11ff7398d44
Connie <138333893+conniechen651@users.noreply.github.com>
2024-01-19 19:54:41 -0800

    Change user input method to buttons instead of typing

--- javascript.js
+++ javascript.js
@@ -2,38 +2,45 @@
 function getComputerChoice(){
     let num = Math.floor(Math.random() * 3);
     if (num==0){
-        return "Rock";
+        return "rock";
     } else if(num==1){
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
+let computerScore = 0;
+let playerScore = 0;
+
 // one round of a game
 function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toString().toLowerCase();
-    computerSelection = computerSelection.toString().toLowerCase();
     if (computerSelection == "rock"){
         if (playerSelection == "rock"){
             return 0;
         } else if (playerSelection == "paper"){
+            playerScore++;
             return 1;
         } else {
+            computerScore++;
             return -1;
         }
     } else if (computerSelection == "paper"){
         if(playerSelection == "rock"){
+            computerScore++;
             return -1;
         } else if (playerSelection == "paper") {
             return 0;
         } else {
+            playerScore++;
             return 1;
         }
     } else {
         if (playerSelection == "rock") {
+            playerScore++;
             return 1;
         } else if (playerSelection == "paper") {
+            computerScore++;
             return -1;
         } else {
             return 0;
@@ -41,34 +48,58 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+const btn = document.querySelector(".buttons");
+const playerScoreText = document.querySelector("#player-score");
+const computerScoreText = document.querySelector("#computer-score");
+const playerChoiceText = document.querySelector("#player-choice");
+const computerChoiceText = document.querySelector("#computer-choice");
 
-// 5 total rounds in a game (ties don't count as a round)
-function game(){
-    let computerScore = 0;
-    let playerScore = 0;
-    let numRounds = 5;
-    let isOver = false;
-    while(!isOver){
-        let computerChoice = getComputerChoice();
-        let round = playRound(prompt("Rock, paper, or scissors?"), computerChoice);
-        console.log("Computer chooses: "+computerChoice);
-        if (round < 0) {
-            computerScore++;
-        } else if (round > 0) {
-            playerScore++;
+btn.addEventListener("click", (event) => {
+    let playerChoice = event.target.id;
+    let computerChoice = getComputerChoice();
+    playRound(playerChoice, computerChoice);
+
+    playerChoiceText.textContent = `You chose: ${playerChoice}`;
+    computerChoiceText.textContent = `Computer chose: ${computerChoice}`;
+
+    playerScoreText.textContent = `Your score: ${playerScore}`;
+    computerScoreText.textContent = `Computer score: ${computerScore}`;
+
+    gameOver();
+    
+});
+
+const results = document.querySelector("#results");
+
+// continue to play rounds until a player reaches 5 points
+function gameOver(){
+    if (playerScore == 5 || computerScore == 5){           
+        if (playerScore > computerScore) {
+            const win = document.createElement("p");
+            win.textContent = "You win!";
+            win.classList.add("win");
+            results.appendChild(win);
         } else {
-            numRounds++;
+            const lose = document.createElement("p");
+            lose.textContent = "You lose!";
+            lose.classList.add("lose");
+            results.appendChild(lose);
         }
-        console.log("Your score: "+playerScore+"      Computer score: "+computerScore);
-        if (playerScore == 5 || computerScore == 5){
-            isOver = true;
-        }
+    const replayBtn = document.createElement("button");
+    replayBtn.classList.add("replay");
+    replayBtn.textContent = "🔁 Replay";
+    results.appendChild(replayBtn);
+
+    btn.removeEventListener();
     }
-    if (playerScore > computerScore) {
-        console.log("You win!");
-    } else {
-        console.log("You lose!");
-    }
+    
 }
 
-console.log(game());
+function replay() {
+    computerScore = 0;
+    playerScore = 0;
+    
+    
+}
+
+replayBtn.addEventListener
